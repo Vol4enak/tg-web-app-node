a5396b95a618fd4f8ecce3ef4bcd94b9f1f14d53
Vol4enak <king5vadim@gmail.com>
2024-05-31 21:31:38 +0300

    change bot key

--- index.js
+++ index.js
@@ -9,21 +9,33 @@ const wedAppUrl = "https://adorable-lebkuchen-d0f7d9.netlify.app";
 const bot = new TelegramBot(token, { polling: true });
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
+bot.on("polling_error", (error) => {
+  console.log(error.code); // => 'EFATAL'
+});
+
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
   if (text === "/start") {
-    await bot.sendMessage(chatId,  "Вітаю у боті в якому ви зможете зручно знайти ваші улюблені товари.", {
-      reply_markup: {
-        keyboard: [
-          [{ text: "заполнить форму", web_app: { url: wedAppUrl + "/form" } }],
-        ],
-      },
-    });
+    await bot.sendMessage(
+      chatId,
+      "Вітаю у боті в якому ви зможете зручно знайти ваші улюблені товари.",
+      {
+        reply_markup: {
+          keyboard: [
+            [
+              {
+                text: "заполнить форму",
+                web_app: { url: wedAppUrl + "/form" },
+              },
+            ],
+          ],
+        },
+      }
+    );
     await bot.sendMessage(chatId, "Заходи к нам", {
       reply_markup: {
         inline_keyboard: [
