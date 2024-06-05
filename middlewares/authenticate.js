d8cad77069a3f7ce5c5f972e06e26a1c766df05d
Vol4enak <king5vadim@gmail.com>
2024-06-06 02:40:22 +0300

    init

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,30 +1,30 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const { User } = require("../models/user");
+// const { User } = require("../models/user");
 
-const { HttpError } = require("../helpers");
+// const { HttpError } = require("../helpers");
 
-const { SECRET_KEY } = process.env;
+// const { SECRET_KEY } = process.env;
 
-const authenticate = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  const [bearer, token] = authorization.split(" ");
+// const authenticate = async (req, res, next) => {
+//   const { authorization = "" } = req.headers;
+//   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer") {
-    next(HttpError(401, "Not authorized"));
-  }
-  try {
-    const { id } = jwt.verify(token, SECRET_KEY);
-    const user = await User.findById(id);
-    if (!user || !user.token || user.token !== token) {
-      next(HttpError(401, "Not authorized"));
-    }
-    req.user = user;
+//   if (bearer !== "Bearer") {
+//     next(HttpError(401, "Not authorized"));
+//   }
+//   try {
+//     const { id } = jwt.verify(token, SECRET_KEY);
+//     const user = await User.findById(id);
+//     if (!user || !user.token || user.token !== token) {
+//       next(HttpError(401, "Not authorized"));
+//     }
+//     req.user = user;
 
-    next();
-  } catch {
-    next(HttpError(401, "Not authorized"));
-  }
-};
+//     next();
+//   } catch {
+//     next(HttpError(401, "Not authorized"));
+//   }
+// };
 
-module.exports = authenticate;
+// module.exports = authenticate;
--- routes/api/products.js
+++ routes/api/products.js
@@ -4,30 +4,30 @@ const router = express.Router();
 
 const { sсhemas } = require("../../models/product");
 
-const { validateBody, isValidId, authenticate } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
 const ctrl = require("../../controllers/products");
 
-router.get("/", authenticate, ctrl.getAll);
+router.get("/", ctrl.getAll);
 
-router.get("/:productsId", authenticate, isValidId, ctrl.getById);
+router.get("/:productsId", isValidId, ctrl.getById);
 
-router.post("/", authenticate, validateBody(sсhemas.addSchema), ctrl.add);
+router.post("/", validateBody(sсhemas.addSchema), ctrl.add);
 
 router.put(
   "/:productsId",
   isValidId,
-  authenticate,
+
   validateBody(sсhemas.addSchema),
   ctrl.updateById
 );
 
 router.patch(
   "/:productsId/products",
-  authenticate,
+
   validateBody(sсhemas.updateStatusProducts),
   ctrl.updateStatus
 );
-router.delete("/:productsId", isValidId, authenticate, ctrl.removeById);
+router.delete("/:productsId", isValidId, ctrl.removeById);
 
 module.exports = router;
