1538289baae9cb9e6dcb82fe4b92b79d62f7f094
Vol4enak <king5vadim@gmail.com>
2024-06-06 16:06:33 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,7 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword }, "-createAt -updatedAt");
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     email: newUser.email,
