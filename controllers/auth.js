f9fd3808253d77b2af38153f5d101f40608e772d
Vol4enak <king5vadim@gmail.com>
2024-06-06 16:37:49 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,14 +41,11 @@ const login = async (req, res) => {
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(user._id, { token });
+//   await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
     token,
-    user: {
-      email: user.email,
-      name: user.name,
-    },
+   
   });
 };
 
