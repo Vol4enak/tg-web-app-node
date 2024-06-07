b5a491e5640e16052875cd4c57295938c0f3870e
Vol4enak <king5vadim@gmail.com>
2024-06-07 16:21:23 +0300

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -3,8 +3,14 @@ const { Product } = require("../models/product");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await Contact.find({});
-  console.log(result);
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Product.find({ owner }, "-createAt -updatedAt", {
+    skip, 
+    limit,
+  }).populate("owner", "email");
+
   res.status(200).json({ result });
 };
 
