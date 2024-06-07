a86e9895cc7b852762de99d3afe3ebdd8611bda4
Vol4enak <king5vadim@gmail.com>
2024-06-07 16:34:42 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+require('dotenv').config();
 const { User } = require("../models/user");
 
 const { HttpError } = require("../helpers");
@@ -11,41 +11,19 @@ const authenticate = async (req, res, next) => {
   const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") {
-    return next(HttpError(401, "Not authorized 1"));
+    next(HttpError(401, "Not authorized 1"));
   }
-
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-
-    if (!user) {
-      console.error();
-      return next(HttpError(401, "User not found with ID:", id));
+    if (!user || !user.token || user.token !== token) {
+      next(HttpError(401, "user not found"));
     }
-
-    if (!user.token) {
-      console.error();
-      return next(HttpError(401, "User has no token:", user));
-    }
-
-    if (user.token !== token) {
-      console.error();
-      return next(
-        HttpError(
-          401,
-          "Token does not match. User token:",
-          user.token,
-          "Provided token:",
-          token
-        )
-      );
-    }
-
     req.user = user;
+
     next();
-  } catch (error) {
-    console.error();
-    next(HttpError(401, "JWT verification failed:", error.message));
+  } catch {
+    next(HttpError(401, "Not authorized 2 "));
   }
 };
 
