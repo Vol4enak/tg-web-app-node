f5aa8826570adf071976d62222202bd3c7aa4253
Vol4enak <king5vadim@gmail.com>
2024-06-09 04:58:17 +0300

    Update productsStore.js

--- controllers/productsStore.js
+++ controllers/productsStore.js
@@ -3,12 +3,7 @@ const { ProductStore } = require("../models/productStore");
 const { ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
-  const result = await ProductStore.find({}, "-createAt -updatedAt", {
-    skip,
-    limit,
-  });
+  const result = await ProductStore.find({}, "-createAt -updatedAt");
 
   res.status(200).json(result);
 };
